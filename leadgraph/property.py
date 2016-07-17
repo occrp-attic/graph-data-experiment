2773a8cf5d9ed27d5b235f9fbfa6ce2b73b6b07f
Friedrich Lindenberg <friedrich@pudo.org>
2016-07-17 16:59:19 +0200

    pad it out with a few bits of practical data & analysis

--- leadgraph/property.py
+++ leadgraph/property.py
@@ -1,3 +1,4 @@
+import six
 import fingerprints
 
 
@@ -10,13 +11,21 @@ class Property(object):
         self.config = config
         self.column = config.get('column')
         self.literal = config.get('literal')
+        self.format = config.get('format')
         self.transform = config.get('transform', '').strip().lower()
         self.key = config.get('key', False)
 
     def bind(self, row):
         value = row.get(self.column, self.literal)
+        if self.format is not None:
+            value = self.format % row
+        if value is None:
+            return
         if self.transform == 'fingerprint':
             value = fingerprints.generate(value)
+        if self.transform == 'lowercase' and \
+                isinstance(value, six.string_types):
+            value = value.lower().strip()
         return value
 
     def __repr__(self):
