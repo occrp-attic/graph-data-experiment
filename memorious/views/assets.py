3d863bbdd6e8dfcacc5434019b0e799dd1df320c
Friedrich Lindenberg <friedrich@pudo.org>
2016-11-08 20:23:53 +0100

    make tooltips work, add some.

--- memorious/views/assets.py
+++ memorious/views/assets.py
@@ -9,7 +9,13 @@ css_assets = Bundle(
     output='assets/style.css'
 )
 
+js_assets = Bundle(
+    'js/util.js',
+    output='assets/app.js'
+)
+
 
 def compile_assets(app):
     assets._named_bundles = {}
     assets.register('css', css_assets)
+    assets.register('js', js_assets)
